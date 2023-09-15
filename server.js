256f32e4e29156b9842a2319c5c789c0f16e63b9
MidoriNoKen <midor1nok3n@gmail.com>
2023-09-15 23:13:26 +0700

    First Commit

---
+++ server.js
@@ -0,0 +1,157 @@
+const http = require("http")
+
+// console.log("Halo, kita akan belajar bagaimana cara membuar server menggunakan nodejs")
+
+/**
+ * Logika untuk menangani dan menanggapi request dituliskan pada fungsi ini
+ * 
+ * @param request: objek yang berisikan informasi terkait permintaan
+ * @param response: objek yang digunakan untuk menanggapi permintaan
+ */
+
+const requestListener = (request, response) => {
+    // const method = request.method
+
+    // response.setHeader('Content-Type', 'text/html');
+    // response.setHeader('X-Powered-By', 'NodeJS')
+    // response.statusCode = 200;
+
+    response.setHeader('Content-Type', 'application/json')
+    response.setHeader('X-Powered-By', 'NodeJS')
+
+    // response.write('<html>')
+    // response.write('<body>')
+    // response.write('<h1>Hello, World!</h1>')
+    // response.write('</body>')
+    // response.write('</html>')
+    // response.end()
+    // response.end('<html><body><h1>Hello, World!</h1></body></html>')
+
+    const { url } = request
+    const { method } = request
+
+    if (url === "/") {
+        // curl http://localhost:5000/
+
+        if (method === "GET") {
+            // response ketika GET
+            response.statusCode = 200
+            
+            response.end(JSON.stringify({
+                message: 'Ini adalah homepage',
+            }));
+        } else {
+            response.statusCode = 404
+            response.statusMessage = "Not Found"
+            response.end(`Halaman tidak dapat diakses dengan ${method} request`)
+        }
+    } else if (url === "/about") {
+        // curl http://localhost:5000/about
+
+        if (method === "GET") {
+            // response ketika GET
+            response.statusCode = 200
+            response.end('Halo! Ini adalah halaman about')
+        }
+        
+
+        if (method === "PUT") {
+            response.statusCode = 200
+            response.end('Dah diubah nih bang... Mau ngapain lagi?')
+        }
+    
+        if (method === "DELETE") {
+            response.statusCode = 200
+            response.end('Yah bang... Dah kehapus loh!')
+        }
+
+        if (method === "POST") {
+            // response ketika POST
+            response.statusCode = 200
+    
+            let body = []
+    
+            request.on('data', (chunk) => {
+                body.push(chunk)
+            })
+    
+            request.on("end", () => {
+                body = Buffer.concat(body).toString()
+                const { name } = JSON.parse(body)
+                response.end(`Halo, ${name}! Ini adalah halaman about`)
+            })
+        } else {
+            response.end(`Halaman tidak dapat diakses dengan ${method} request`)
+            response.statusCode = 404
+            response.statusMessage = "Not Found"
+        }
+    } else {
+        response.statusCode = 404
+        response.statusMessage = "Not Found"
+        response.end(JSON.stringify({
+            message: `Halaman ${url} tidak dapat ditemukan!`
+        }))
+    }
+
+    // curl http://localhost:5000/<any>
+
+    // if (method === "GET") {
+    //     // response ketika GET
+    //     response.end('<h1>Welcome Guys!</h1>')
+    // }
+
+    // if (method === "POST") {
+    //     // response ketika POST
+    //     response.end('<h2>Hayo... Mau ngapain?</h2>')
+    // }
+
+    // if (method === "PUT") {
+    //     response.end('Dah diubah nih bang... Mau ngapain lagi?')
+    // }
+
+    // if (method === "DELETE") {
+    //     response.end('Yah bang... Dah kehapus loh!')
+    // }
+
+    // Anda bisa mengevaluasi tipe method lainnya
+    
+    // response.setHeader('Content-Type', 'text/html')
+
+    // response.statusCode = 200
+    // response.end("<h1>Halo HTTP Server!</h1>")
+
+    // if (method === "POST") {
+    //     // response ketika POST
+
+    //     let body = []
+
+    //     request.on('data', (chunk) => {
+    //         body.push(chunk)
+    //     })
+
+    //     request.on("end", () => {
+    //         body = Buffer.concat(body).toString()
+    //         const { name } = JSON.parse(body)
+    //         response.end(`<h2>Hayo... Mau ngapain ${name}?</h2>`)
+    //     })
+    // }
+
+    // let body = []
+
+    // request.on('data', (chunk) => {
+    //     body.push(chunk)
+    // })
+
+    // request.on("end", () => {
+    //     body = Buffer.concat(body).toString()
+    // })
+}
+
+const server = http.createServer(requestListener)
+
+const port = 5000
+const host = 'localhost'
+
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`)
+})
